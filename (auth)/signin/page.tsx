23ea9078d1f335dd744dd41431ecb759c036a3d8
Stefan Djordjevic <stefan@Stefans-MacBook-Pro.local>
2024-11-03 00:04:04 -0400

    more changes

--- (auth)/signin/page.tsx
+++ (auth)/signin/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "Sign In - Open PRO",
+  title: "Sign In - McKinsly",
   description: "Page description",
 };
 
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "McKinsly - Take back your time",
+  description: "",
 };
 
 import PageIllustration from "@/components/page-illustration";
